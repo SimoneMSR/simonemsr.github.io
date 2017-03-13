474cbf8caf1d969f3190d6fbcef0a348bca87e6f
Simone D'Amario <simone.damario@kalpa.it>
2017-03-13 18:14:40 +0100

    svg autonomo

--- hokey/code.js
+++ hokey/code.js
@@ -4,9 +4,6 @@
 	function run(){
 		var anim = document.getElementById("gesu_ciuffo");
 		anim.addEventListener("animationiteration", AnimationListener, false);
-		$('#gesu_ciuffo').on('webkitAnimationEnd mozAnimationEnd msAnimationEnd oAnimationEnd animationEnd', function(e) {
-			$(this).css({'animation-duration': Math.random() + 1 + 's'},0);
-		});
 	}
 
 	function AnimationListener(){
